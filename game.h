9e14ea383d455e3afe065f8437925b391248ec8f
xyyuan1 <60038061+xyyuan1@users.noreply.github.com>
2020-06-12 14:35:19 +0800

    Update game.h

--- game.h
+++ game.h
@@ -1,5 +1,6 @@
-class game {
-	void gamestart(); //开始游戏
-	void gameover();  //游戏结束 
-	void score(); //统计分数
+class game
+{
+public:
+    void gamestop(HANDLE hOut, int block1[4][4], int block2[4][4], Block& b);//暂停游戏
+    void gameover(HANDLE hOut, player& p, Block& b, int block1[4][4], int block2[4][4]); //游戏结束
 };
